f2a3f96d47eff5558f176baac0aa920839a1693a
cq179697 <bondook714@yahoo.com>
2016-09-30 10:40:05 -0500

    Add files via upload

---
+++ MultipleForms/MultipleForms/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class MessageForm
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        'Close the form
+        Me.Close()
+    End Sub
+End Class
---
+++ MultipleForms/MultipleForms/MainForm.vb
@@ -0,0 +1,17 @@
+﻿Public Class MainForm
+
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+        'Create an instance of MessageForm 
+        Dim frmMessage As New MessageForm
+
+        'Display the form in model style 
+        frmMessage.ShowDialog()
+
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+
+    End Sub
+End Class
